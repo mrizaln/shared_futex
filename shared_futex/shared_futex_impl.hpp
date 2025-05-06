a6f2061b21a16627d7d40fb2afaff62d2800de46
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-06 12:02:37 +0700

    Move static variable definition to header

--- shared_futex/shared_futex_impl.hpp
+++ shared_futex/shared_futex_impl.hpp
@@ -1,5 +1,7 @@
 // shared_futex
 // © Shlomi Steinberg, 2015-2018
+//
+// modified by Muhammad Rizal Nurromdhoni, 2025
 
 #pragma once
 
@@ -193,7 +195,19 @@ struct random_generator {
 private:
 	struct seeded_mt19937 {
 		std::mt19937 rand_engine;
-		seeded_mt19937() noexcept;
+		seeded_mt19937() noexcept : rand_engine(generate_seeded_random_engine()) {}
+
+        static std::mt19937 generate_seeded_random_engine() noexcept {
+			const auto tse = std::chrono::high_resolution_clock::now().time_since_epoch();
+			const auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(tse).count();
+			const auto tid = std::hash<std::thread::id>{}(std::this_thread::get_id());
+
+			// Construct a seed sequence with two sources of entropy, time since epoch and thread id.
+			std::seed_seq seq = { static_cast<std::uint64_t>(time), static_cast<std::uint64_t>(tid) };
+
+			// Seed the Mersenne Twister engine
+			return std::mt19937(seq);
+		}
 	} gen;
 
 public:
